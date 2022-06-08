b87fbe6abd24444550ef293a56c9bbe9c1dd6023
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-06-08 15:13:18 +0500

    1

---
+++ DZ_1.cs
@@ -0,0 +1,57 @@
+﻿namespace CS_DZ_Rasp_1
+{
+    class Weapon
+    {
+        private readonly int _damage;
+        private int _bullets;
+
+        private void DoDamage()
+        {
+            _bullets--;
+        }
+
+        public bool TryAttackPlayer(Player player)
+        {
+            if (_bullets > 0 && _damage > 0)
+            {
+                if (player.CheckPlayerAlive())
+                {
+                    DoDamage();
+                    player.TakeDamage(_damage);
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }
+    }
+
+    class Player
+    {
+        private int _health;
+
+        public void TakeDamage(int damage)
+        {
+            _health -= damage;
+        }
+
+        public bool CheckPlayerAlive()
+        {
+            if (_health < 0)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+
+    class Bot
+    {
+        private Weapon _weapon;
+
+        public void OnSeePlayer(Player player)
+        {
+            _weapon.TryAttackPlayer(player);
+        }
+    }
+}
