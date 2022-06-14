3db461258ec9339296afe76d22c0a8027cb0d6c4
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-06-14 15:29:27 +0500

    Add

---
+++ SimpleEnglish.cs
@@ -0,0 +1,14 @@
+﻿namespace Clean_Code_2
+{
+    class Program
+    {
+        public static int Find(int[] array, int element)
+        {
+            for (int i = 0; i < array.Length; i++)
+                if (array[i] == element)
+                    return i;
+
+            return -1;
+        }
+    }
+}
