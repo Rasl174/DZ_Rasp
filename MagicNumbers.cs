f571c9a7e99cefbe97182cbe8173b93a39adac8f
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-06-14 15:44:21 +0500

    Add

---
+++ MagicNumbers.cs
@@ -0,0 +1,13 @@
+﻿namespace Clean_Code_3
+{
+    class Weapon
+    {
+        public const int BulletsPerShot = 1;
+
+        private int _bullets;
+
+        public bool CanShoot() => _bullets >= BulletsPerShot;
+
+        public void Shoot() => _bullets -= BulletsPerShot;
+    }
+}
