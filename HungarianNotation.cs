881d93239daa90edb86829fe6097d8c7e4ffa4c5
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-06-14 15:55:22 +0500

    Add

---
+++ HungarianNotation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clean_code_4
+{
+    class Program
+    {
+        private readonly int _armySize = 10;
+        private readonly int _coinsCount = 10;
+        private readonly string _name = "Vladislav";
+    }
+}
