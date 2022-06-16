beb07264010f8544c5f1825856d72379e0968107
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-06-16 13:53:41 +0500

    Add

---
+++ Arguments.cs
@@ -0,0 +1,15 @@
+﻿namespace Clean_code_11
+{
+    class Program
+    {
+        public void Enable()
+        {
+            _effects.StartEnableAnimation();
+        }
+
+        public void Disable()
+        {
+            _pool.Free(this);
+        }
+    }
+}
